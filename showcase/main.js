f490a24b70984c45199e8b3537d98c82cec9ec2e
kreuzerk <kevin.kreuzer90@icloud.com>
2021-01-26 11:58:59 +0100

    fix: 🐛 add missing main.js

---
+++ showcase/main.js
@@ -0,0 +1,34 @@
+import {completeIconSet} from '../dist/icons';
+
+function buildIconCard(icon) {
+    const iconCard = document.createElement('div');
+    iconCard.classList.add('icon-card');
+    iconCard.appendChild(buildSVGElement(icon));
+    iconCard.appendChild(document.createTextNode(icon.name));
+    return iconCard;
+}
+
+function buildSVGElement(icon) {
+    const div = document.createElement('DIV');
+    div.innerHTML = icon.data;
+    return (
+        div.querySelector('svg') ||
+        this.document.createElementNS('http://www.w3.org/2000/svg', 'path')
+    );
+
+}
+
+function buildIconList(iconSet) {
+    const iconList = document.querySelector('.icon-list');
+    iconList.innerHTML = '';
+    iconSet.forEach(icon => iconList.appendChild(buildIconCard(icon)));
+}
+
+const searchField = document.querySelector('input');
+searchField.addEventListener('keydown', function (event) {
+    const newIconSet = completeIconSet.filter(icon => icon.name.includes(searchField.value));
+    buildIconList(newIconSet);
+});
+
+buildIconList(completeIconSet);
+
